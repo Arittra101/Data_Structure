db00437c40cb8dc083cc1c942e574d12306b31dc
Avishek202 <avishek51775@gmail.com>
2021-07-04 18:54:57 +0600

    list show

--- Algorithm Lab_02_(Graph)/dfs.cpp
+++ Algorithm Lab_02_(Graph)/dfs.cpp
@@ -5,7 +5,9 @@ typedef long long int lli;
 lli i,j;
 lli v[100][100];
 vector<lli>graph[100];
+
 lli visited[100];
+lli activate[100];
 
 void dfs(lli start)
 {
@@ -18,7 +20,7 @@ void dfs(lli start)
         lli x = s.top();
         cout<<x<<" ";
         s.pop();
-        for(i=0;i<graph[x].size();i++)
+        for(i=0; i<graph[x].size(); i++)
         {
             if(visited[graph[x][i]]==0)
             {
@@ -31,21 +33,44 @@ void dfs(lli start)
 
 
 }
+void printVector(vector<lli>&v)
+{
+    for(auto it1 :  v)
+        cout<<"->"<<it1;
+    cout<<endl;
+}
 int main()
 {
+
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
     lli edge;
     cin>>edge;
-    for(lli i=0;i<edge;i++)
+    set<lli>s;
+    for(lli i=0; i<edge; i++)
     {
         lli x,y;
         cin>>x>>y;
+        activate[x]=1;
+        activate[y]=1;
         graph[x].push_back(y);
         graph[y].push_back(x);
     }
-    dfs(10);
+    //dfs(10);
+
+
+    for(i=0; i<100; i++)
+    {
+        if(activate[i]==1)
+        {
+            cout<<"Node "<<i<<" :";
+             printVector(graph[i]);
+        }
+
+    }
+
+
     return 0;
 }
 
